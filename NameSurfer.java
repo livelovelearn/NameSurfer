e6fe61ce4c45fcb38baa6303d82ea9ca5f16b851
S <s>
2013-08-21 22:09:19 -0400

    {"type":"TI", "runs":"1"}

--- NameSurfer.java
+++ NameSurfer.java
@@ -19,6 +19,7 @@ public class NameSurfer extends ConsoleProgram implements NameSurferConstants {
  */
 	public void init() {
 	    // You fill this in, along with any helper methods //
+		JButton button1 = new JButton ("hello");
 	}
 
 /* Method: actionPerformed(e) */
