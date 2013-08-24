5303f36c5e00cfeec355e5b54e7e55a199067b63
S <s>
2013-08-24 12:17:58 -0400

    {"type":"TI", "runs":"1"}

--- NameSurferEntry.java
+++ NameSurferEntry.java
@@ -20,26 +20,24 @@ public class NameSurferEntry implements NameSurferConstants {
 	 */
 	public NameSurferEntry(String line) {
 		// You fill this in //
-		int current =0;
-		//rank = new int[11];
-		
+		int current = 0;
+		// rank = new int[11];
+
 		for (int i = 0; i < line.length(); i++) {
 			if (line.charAt(i) == ' ') {
 				current = i;
 				name = line.substring(0, i);
 				break;
 			}
-			}
-			
-		
-		
+		}
+
 		for (int i = 0; i < 11; i++) {
 			String srank = null;
 			System.out.println("line position" + current);
 			for (int j = current + 1; j < line.length(); j++) {
 				if (line.charAt(j) != ' ') {
 					srank += line.charAt(j);
-					} else {
+				} else {
 					current = j;
 					rank[i] = Integer.parseInt(srank);
 					break;
@@ -56,7 +54,6 @@ public class NameSurferEntry implements NameSurferConstants {
 		// You need to turn this stub into a real implementation //
 		return name;
 	}
-	
 
 	/* Method: getRank(decade) */
 	/**
@@ -65,7 +62,7 @@ public class NameSurferEntry implements NameSurferConstants {
 	 * the first year in the database, which is given by the constant
 	 * START_DECADE. If a name does not appear in a decade, the rank value is 0.
 	 */
-	/*public int getRank(int decade) {
+	public int getRank(int decade) {
 		// You need to turn this stub into a real implementation //
 		int a = (decade - START_DECADE) / 10;
 		return rank[a];
@@ -76,16 +73,16 @@ public class NameSurferEntry implements NameSurferConstants {
 	 * Returns a string that makes it easy to see the value of a
 	 * NameSurferEntry.
 	 */
-	
+
 	public String toString() {
 		// You need to turn this stub into a real implementation //
-		return "here3";
-		/*return (name + " [" + rank[0] + " " + rank[1] + " " + rank[2] + " "
+
+		return (name + " [" + rank[0] + " " + rank[1] + " " + rank[2] + " "
 				+ rank[3] + " " + rank[4] + " " + rank[5] + " " + rank[6] + " "
-				+ rank[7] + " " + rank[8] + " " + rank[9] + " " + rank[10] + " ");*/
+				+ rank[7] + " " + rank[8] + " " + rank[9] + " " + rank[10] + " ");
 	}
 
 	private String name;
 	private int[] rank;
-	
+
 }
