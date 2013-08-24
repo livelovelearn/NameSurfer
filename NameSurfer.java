5f4c9f1f76424068c7e69f14dbac7078adb3f9fc
S <s>
2013-08-24 18:49:59 -0400

    {"type":"TI", "runs":"2"}

--- NameSurfer.java
+++ NameSurfer.java
@@ -11,54 +11,50 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class NameSurfer extends Program implements NameSurferConstants {
-	
 
-/* Method: init() */
-/**
- * This method has the responsibility for reading in the data base
- * and initializing the interactors at the top of the window.
- */
-	
+	/* Method: init() */
+	/**
+	 * This method has the responsibility for reading in the data base and
+	 * initializing the interactors at the top of the window.
+	 */
+
 	public void init() {
-	    // You fill this in, along with any helper methods //
-		
+		// You fill this in, along with any helper methods //
+
 		NSD = new NameSurferDataBase(NAMES_DATA_FILE);
 		add(new JLabel("Name"), NORTH);
 		add(nameField, NORTH);
-		add(new JButton ("Graph"), NORTH);
-		add(new JButton ("Clear"), NORTH);
+		add(new JButton("Graph"), NORTH);
+		add(new JButton("Clear"), NORTH);
 		graph = new NameSurferGraph();
 		add(graph);
-		//graph.update();
+		// graph.update();
 		addActionListeners();
-		
+
 	}
 
-/* Method: actionPerformed(e) */
-/**
- * This class is responsible for detecting when the buttons are
- * clicked, so you will have to define a method to respond to
- * button actions.
- */
+	/* Method: actionPerformed(e) */
+	/**
+	 * This class is responsible for detecting when the buttons are clicked, so
+	 * you will have to define a method to respond to button actions.
+	 */
 	public void actionPerformed(ActionEvent e) {
-		
+
 		String cmd = e.getActionCommand();
-		NSE = NSD.findEntry(nameField.getText());
-		graph.addEntry(NSE);
-		
-		if (cmd.equals("Graph"))
-		{if (NSE==null) 
-			println("invalid input");
-		else 
-			println("Name you entered " + NSE.toString());
+
+		if (cmd.equals("Graph")) {
+			NSE = NSD.findEntry(nameField.getText());
+			if (NSE != null)
+				graph.addEntry(NSE);
+
 		}
 		if (cmd.equals("Clear"))
-			println("clear");
+			graph.update();
 	}
+
 	private JTextField nameField = new JTextField(20);
 	private NameSurferDataBase NSD;
 	private NameSurferEntry NSE;
 	private NameSurferGraph graph;
-
 
 }
